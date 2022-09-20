2897b06a92d7310bc5270cb098c7acf8cbb7804e
schwarzer79 <sungamo2@naver.com>
2022-09-20 14:38:27 +0900

    new

--- chapter3_regression.py
+++ chapter3_regression.py
@@ -73,3 +73,65 @@ knr.n_neighbors = 3
 knr.fit(train_input, train_target)
 print(knr.score(test_input, test_target)) #0.9746459963987609
 print(knr.score(train_input, train_target)) # 0.9804899950518966 -> train을 넣은 경우가 더 크고 둘 사이의 차이가 크지 않으므로 과소적합도 아니고 과대적합 또한 아니다
+
+### 선형 회귀
+import numpy as np
+perch_length = np.array([8.4, 13.7, 15.0, 16.2, 17.4, 18.0, 18.7, 19.0, 19.6, 20.0, 21.0,
+       21.0, 21.0, 21.3, 22.0, 22.0, 22.0, 22.0, 22.0, 22.5, 22.5, 22.7,
+       23.0, 23.5, 24.0, 24.0, 24.6, 25.0, 25.6, 26.5, 27.3, 27.5, 27.5,
+       27.5, 28.0, 28.7, 30.0, 32.8, 34.5, 35.0, 36.5, 36.0, 37.0, 37.0,
+       39.0, 39.0, 39.0, 40.0, 40.0, 40.0, 40.0, 42.0, 43.0, 43.0, 43.5,
+       44.0])
+perch_weight = np.array([5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 85.0, 110.0,
+       115.0, 125.0, 130.0, 120.0, 120.0, 130.0, 135.0, 110.0, 130.0,
+       150.0, 145.0, 150.0, 170.0, 225.0, 145.0, 188.0, 180.0, 197.0,
+       218.0, 300.0, 260.0, 265.0, 250.0, 250.0, 300.0, 320.0, 514.0,
+       556.0, 840.0, 685.0, 700.0, 700.0, 690.0, 900.0, 650.0, 820.0,
+       850.0, 900.0, 1015.0, 820.0, 1100.0, 1000.0, 1100.0, 1000.0,
+       1000.0])
+
+from sklearn.model_selection import train_test_split
+
+train_input, test_input, train_target, test_target = train_test_split(perch_length, perch_weight, random_state = 42)
+train_input = train_input.reshape(-1,1)
+test_input = test_input.reshape(-1,1)
+
+from sklearn.neighbors import KNeighborsRegressor
+
+knr = KNeighborsRegressor(n_neighbors = 3)
+knr.fit(train_input, train_target)
+
+print(knr.predict([[50]])) # 1033g으로 무게 예측 --> 예측값과 실제값의 큰 차이가 발생 why?
+
+# 무게 오차의 원인을 알기 위해 산점도 작성
+import matplotlib.pyplot as plt
+
+distances, indexes = knr.kneighbors([[50]])
+plt.scatter(train_input, train_target)
+plt.scatter(train_input[indexes], train_target[indexes], marker = 'D')
+plt.scatter(50, 1033, marker = '^')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show(block=True) # 산점도 결과 길이 50인 도미와 가장 가까운 점들이 45cm 근방이기에 k-nearest 알고리즘은 이 부근의 평균을 출력할 수 밖에 없음 --> 때문에 결과가 실제값과 달라짐
+print(np.mean(train_target[indexes])) # k-nearest의 특성 상 새로운 값이 기존의 범위를 벗어나면 엉뚱한 값을 예측할 수 밖에 없다 --> 현재 데이터셋에서는 도미의 길이가 아무리 늘어나도 무게 예측값이 항상 1033g으로 나올 수 밖에 없음 --> 다른 알고리즘이 필요
+
+# 선형 회귀
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+
+lr.fit(train_input, train_target)
+print(lr.predict([[50]]))
+print(lr.coef_, lr.intercept_) # lr 모형을 이용해 fitting한 이후 모델의 계수값과 절편값 보기(parameter)
+
+plt.scatter(train_input, train_target)
+plt.plot([15,50], [15*lr.coef_ + lr.intercept_, 50*lr.coef_ + lr.intercept_]) # 15~50까지의 1차방정식 그래프 --> 이 직선이 선형회귀에서 찾은 데이터셋의 최적 직선
+plt.scatter(50, 1241.8, marker = '^')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show(block=True)
+
+print(lr.score(train_input, train_target)) #0.939846333997604
+print(lr.score(test_input, test_target)) #0.8247503123313558
+
+# 다항회귀
+
